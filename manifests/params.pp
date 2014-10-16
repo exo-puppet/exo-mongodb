f2af91f5819f7eb074072dbd550c9c7158e0acbf
Frederic DROUET <fdrouet@exoplatform.com>
2014-10-16 11:32:43 +0200

    Fix the MongoDB service name which is different between 2.4 and 2.6

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,14 +1,14 @@
 class mongodb::params {
   case $::operatingsystem {
     /(Ubuntu)/ : {
-      $service_name    = 'mongodb'
-
       # For MongoDB 2.4 and before
+      $service_name_24    = 'mongodb'
       $package_name_24 = 'mongodb-10gen'
       $template_config_24 = 'mongodb/etc/mongodb.conf-10gen.erb'
       $config_file_24     = '/etc/mongodb.conf'
 
       # For MongoDB 2.6 and after
+      $service_name_26    = 'mongod'
       $package_name_26 = 'mongodb-org'
       $template_config_26 = 'mongodb/etc/mongodb.conf-org.erb'
       $config_file_26     = '/etc/mongod.conf'
--- manifests/service.pp
+++ manifests/service.pp
@@ -3,7 +3,11 @@ class mongodb::service {
 
   service { 'mongodb':
     ensure     => running,
-    name       => $mongodb::params::service_name,
+    name       => $mongodb::package_name ? {
+      $mongodb::params::package_name_24 => $mongodb::params::service_name_24,
+      $mongodb::params::package_name_26 => $mongodb::params::service_name_26,
+      default => fail("The specified MongoDB package name is not managed (${mongodb::package_name})")
+    },
     hasstatus  => true,
     hasrestart => true,
     require    => Class['mongodb::config'],
